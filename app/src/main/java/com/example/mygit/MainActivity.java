3c868a872dc7f887478c90004a845522c000e613
Pan <1737372480@qq.com>
2019-11-20 15:51:54 +0800

    this first

--- app/src/main/java/com/example/mygit/MainActivity.java
+++ app/src/main/java/com/example/mygit/MainActivity.java
@@ -7,9 +7,13 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void fun1(){
+        Log.d(TAG, "fun1: ");
     }
 }
